82a7a5fff955a9d2bbb98440543a6108bf4d4226
chiugame <43848388+chiugame@users.noreply.github.com>
2024-02-11 13:52:55 +0900

    インデントがTabと半角空白が混在していたのでTabに統一

--- Packages/si.iwa.udontask/Runtime/UdonTask.cs
+++ Packages/si.iwa.udontask/Runtime/UdonTask.cs
@@ -4,37 +4,37 @@ using VRC.Udon;
 
 namespace Iwashi.UdonTask
 {
-    [AddComponentMenu("")]
-    public class UdonTask : UdonSharpBehaviour
-    {
-        private static UdonAsync GetUdonAsync(string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
-        {
-            var udonAsyncObj = GameObject.Find("0C74D46CC893548DFFAF93B6D0C59BCDC2909B0F2438E978F6B5ED10E05F290B");
-            var obj = Instantiate(udonAsyncObj.transform.Find("Prefab").gameObject);
-            var udonAsync = obj.GetComponent<UdonAsync>();
-            udonAsync.taskMethodName = taskMethodName;
-            udonAsync.onFailedMethodName = onFailedMethodName;
-            udonAsync.onSuccessMethodName = onSuccessMethodName;
-            udonAsync.timeout = timeout;
-            return udonAsync;
-        }
-
-        public static UdonTask New(UdonBehaviour udonBehaviour, string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
-        {
-            var udonAsync = GetUdonAsync(taskMethodName, onFailedMethodName, onSuccessMethodName, timeout);
-            udonAsync.udonBehaviour = udonBehaviour;
-            udonAsync.gameObject.SetActive(true);
-            udonAsync.isUdonBehaviour = true;
-            return (UdonTask)(object)new object[] { udonAsync };
+	[AddComponentMenu("")]
+	public class UdonTask : UdonSharpBehaviour
+	{
+		private static UdonAsync GetUdonAsync(string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
+		{
+			var udonAsyncObj = GameObject.Find("0C74D46CC893548DFFAF93B6D0C59BCDC2909B0F2438E978F6B5ED10E05F290B");
+			var obj = Instantiate(udonAsyncObj.transform.Find("Prefab").gameObject);
+			var udonAsync = obj.GetComponent<UdonAsync>();
+			udonAsync.taskMethodName = taskMethodName;
+			udonAsync.onFailedMethodName = onFailedMethodName;
+			udonAsync.onSuccessMethodName = onSuccessMethodName;
+			udonAsync.timeout = timeout;
+			return udonAsync;
 		}
 
-        public static UdonTask New(UdonSharpBehaviour udonSharpBehaviour, string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
-        {
-            var udonAsync = GetUdonAsync(taskMethodName, onFailedMethodName, onSuccessMethodName, timeout);
-            udonAsync.udonSharpBehaviour = udonSharpBehaviour;
-            udonAsync.gameObject.SetActive(true);
-            udonAsync.isUdonSharp = true;
-            return (UdonTask)(object)new object[] { udonAsync };
+		public static UdonTask New(UdonBehaviour udonBehaviour, string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
+		{
+			var udonAsync = GetUdonAsync(taskMethodName, onFailedMethodName, onSuccessMethodName, timeout);
+			udonAsync.udonBehaviour = udonBehaviour;
+			udonAsync.gameObject.SetActive(true);
+			udonAsync.isUdonBehaviour = true;
+			return (UdonTask)(object)new object[] { udonAsync };
 		}
-    }
+
+		public static UdonTask New(UdonSharpBehaviour udonSharpBehaviour, string taskMethodName, string onFailedMethodName = "", string onSuccessMethodName = "", float timeout = 30f)
+		{
+			var udonAsync = GetUdonAsync(taskMethodName, onFailedMethodName, onSuccessMethodName, timeout);
+			udonAsync.udonSharpBehaviour = udonSharpBehaviour;
+			udonAsync.gameObject.SetActive(true);
+			udonAsync.isUdonSharp = true;
+			return (UdonTask)(object)new object[] { udonAsync };
+		}
+	}
 }
--- Packages/si.iwa.udontask/Runtime/UdonTaskExt.cs
+++ Packages/si.iwa.udontask/Runtime/UdonTaskExt.cs
@@ -1,15 +1,15 @@
 namespace Iwashi.UdonTask
 {
-    public static class UdonTaskExt
-    {
-        public static UdonAsync GetAsync(this UdonTask self)
-        {
-            var objs = (object[])(object)self;
-            return (UdonAsync)objs[0];
-        }
+	public static class UdonTaskExt
+	{
+		public static UdonAsync GetAsync(this UdonTask self)
+		{
+			var objs = (object[])(object)self;
+			return (UdonAsync)objs[0];
+		}
 
-        public static bool IsComplete(this UdonTask self) => self.GetAsync().IsComplete();
+		public static bool IsComplete(this UdonTask self) => self.GetAsync().IsComplete();
 
-        public static ResultType ResultStatus(this UdonTask self) => self.GetAsync().ResultStatus();
-    }
+		public static ResultType ResultStatus(this UdonTask self) => self.GetAsync().ResultStatus();
+	}
 }
